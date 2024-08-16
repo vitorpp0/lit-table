f6f86132f9eac5e81917836b42dee3b955784dbd
Vitor Pinheiro Pinto <vitorpp0@hotmail.com>
2024-08-16 04:56:27 -0300

    NEW FEATURE: Webpack's .html template and entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import "./simple-greeting"
+import "./lit-table-tree/lit-table-tree";
+import "./simple-lit-table/simple-lit-table"
 
-const el  =  document.createElement("simple-greeting");
+document.body.style.backgroundColor = "#1D242B";
+
+let el:HTMLElement  =  document.createElement("lit-table-tree");
+document.body.appendChild(el);
+
+el  =  document.createElement("simple-lit-table");
 document.body.appendChild(el);
