17c408ca683c1a86c6aadcd35a69979f8d7a7d2f
Vitor Pinheiro Pinto <vitorpp0@hotmail.com>
2024-09-17 23:32:03 -0300

    REFACT: A more view like component

--- src/lit-table-tree/lit-table-tree.ts
+++ src/lit-table-tree/lit-table-tree.ts
@@ -1,109 +1,108 @@
-import {LitElement, html, nothing, unsafeCSS, TemplateResult} from 'lit';
+import {LitElement, html, nothing, unsafeCSS, TemplateResult, PropertyValues} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 
 import styleCSS from "./style.scss";
-import data from "./local_data.json";
-
-import {TableConfig, TableColumn, TableRow} from './lit-table-tree.d'
 
 import '@polymer/paper-icon-button/paper-icon-button'
 import '@polymer/iron-icons/iron-icons';
 import '@polymer/paper-icon-button/paper-icon-button';
 
+import { ProcessController, ProcessControllerAggRules, Operation } from './controllers/preprocessController';
+
 @customElement('lit-table-tree')
 export class LitTableTree extends LitElement {
-    @property({attribute:"data", type:Object}) tableData:TableConfig = data as TableConfig;
+    @property({attribute:"title", type:String}) title:string = 'Default Title';
+    @property({attribute:"headers", type:Object}) headers:Array<string> = ['col1', 'col2', 'col3']; 
+    @property({attribute:"data", type:Object}) data:Array<any>;
+    @property({attribute:'agg-rules', type:Object}) aggRules:ProcessControllerAggRules;
     @state() subheaderIconButton:String = "icons:arrow-drop-up"
     @state() showSubheader:Boolean = false;
 
-    static styles = unsafeCSS(styleCSS);
+    private processController = new ProcessController(this);
+ 
+    static styles = [unsafeCSS(styleCSS)];
 
     constructor(){super();}
+
+    protected willUpdate(_changedProperties: PropertyValues): void {
+        if(_changedProperties.has('data')){
+            this.processController.setDataframe();
+            this.processController.updateTable();
+            console.log(this.processController.table);
+        }
+        if(_changedProperties.has('aggRules')){
+            this.processController.setAggRules();
+            this.processController.updateTable();
+            console.log(this.processController.table);
+        }
+    }
 
     render() {
         return html`
             <div id="title-train">
-                <p id="title">${this.tableData.title}</p>
+                <p id="title">${this.title}</p>
                 <div id="button-train">
                     <slot></slot>
-                    ${this.getDefaultButtons(this.tableData)}
                 </div>
             </div>
             <table>
-                ${this.getTableHead(this.tableData.headers, "main")}
-                ${this.getTableHead(this.tableData.subheaders, "sub")}        
-                ${this.tableData.data.map((row:TableRow, idx)=>{return this.getHTMLRow(row,idx+1,0)})}
+                ${this.renderHeaders()}
+                ${this.renderData()}
             </table>
         `;
     }
 
-    // render support
-    getDefaultButtons(config:TableConfig){
-        return html`
-            ${
-                config.subheaders 
-                    ? html`
-                        <paper-icon-button 
-                            icon="icons:arrow-drop-${this.showSubheader ? "up" : "down"}"
-                            @click=${this.showOrHideSubheader}
-                        ></paper-icon-button>` 
-                    : nothing
-            }
-            <paper-icon-button 
-                icon="icons:info-outline"
-            ></paper-icon-button>
-        `;
+    renderHeaders(){
+        return this.headers 
+            ? html`<tr class="header">
+                    ${this.headers.map(col=>html`<th>${col}</th>`)}
+                </tr>`
+            : nothing;
     }
 
-    getTableHead(head:Array<TableColumn>, type:"main"|"sub"){
-        const tr_class = type=="main" ? "header" : "subheader";
-        if(head.length==0){
-            return nothing;
-        }else{
-            return html`
-                <tr class="${tr_class}" ?show=${this.showSubheader}>
-                    ${head.map((header:TableColumn)=>html`<th>${header.value}</th>`)}
-                </tr>
-            `
-        }
-    }
-
-    getHTMLRow(row:TableRow,row_idx:number=0,level:number=0):TemplateResult{
-        const test = `${level==0 ? "" : "hidden"} data`
-        return html`
-            <tr id="row${row_idx}" class="${test}" parent_row="${row_idx}">
-                ${
-                    row.parent.map((data:any, idx:number)=>{
-                        const test2 = level>0 && idx==0 ? `padding-left:${24+level*10}px` : nothing;
-                        return html`<td style="${test2}">
-                            ${
-                                (idx==0 && row.children.length>0) 
-                                ?  html`
-                                    <iron-icon
-                                        icon="icons:arrow-drop-down"
-                                        @click=${(e:any)=>{this.showHideRows(e,{row:row_idx,children:row.children.length})}}
-                                    ></iron-icon>` 
-                                : nothing
+    renderData(){
+        if(this.processController.table){
+            return this.processController.table.map((row, idx)=>{
+                const props = Object.keys(row).filter((key:string)=>!key.includes('ctrl'))
+                let hasIcon = false;
+                return html`<tr class="data">
+                    ${
+                        props.map((prop: keyof typeof row, propIdx)=>{
+                            if(row[prop]!==null){
+                                if(!hasIcon && row.ctrl_expansible){
+                                    hasIcon=true;
+                                    const op:Operation = {
+                                        value:`${row[prop]}`,
+                                        prop:propIdx,
+                                        target:idx,
+                                        parent: propIdx==0 ? null : propIdx-1
+                                    };
+                                    return html`
+                                        <td>
+                                            <iron-icon
+                                                icon="icons:arrow-drop-down"
+                                                @click=${(e:any)=>{this.handleExpand(e,op)}}
+                                            ></iron-icon> 
+                                            ${row[prop]}
+                                        </td>`
+                                }else{
+                                    return html`<td>${row[prop]}</td>`
+                                }
                             }
-                            ${data}
-                        </td>`
-                    })
-                }
-            </tr>
-            ${row.children.map((childRow, idx)=>this.getHTMLRow(childRow,row_idx*10+idx+1,level+1))}
-        `
-    }
-
-    // events
-    showOrHideSubheader(){
-        this.showSubheader = !this.showSubheader;
-    }
-
-    showHideRows(e:any, detail:{row:number, children:number}){
-        e.target.icon = e.target.icon == "icons:arrow-drop-down" ? "icons:arrow-drop-up" : "icons:arrow-drop-down" ;
-        for(let i=0; i<detail.children; i++){
-            let child:HTMLTableRowElement = this.renderRoot.querySelector(`#row${10*detail.row+i+1}`);
-            child.style.display= child.style.display == "none" || child.style.display == ""  ? "table-row" : "none";
+                            return html`<td></td>`
+                        })
+                    }
+                </tr>`
+            })
         }
+        return nothing;
+    }
+
+    handleExpand(e:any, obj:Operation){
+        console.log(obj);
+        this.processController.setOperation(obj);
+        // this.processController.operations.push(obj);
+        // this.processController.updateTable();
+        // this.requestUpdate();
     }
 }
